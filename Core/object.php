d0f32b38b37931cf8816e22ed2f4f88b1cc3496e
quangnd512 <quangnd512@gmail.con>
2020-07-09 23:05:30 +0700

    update code php oop

--- Core/object.php
+++ Core/object.php
@@ -1,39 +1,11 @@
 <?php
-class Cha
+class Caculator
 {
-    public $tong;
-    public $c = 5;
-    function Tong($a, $b)
-    {
-        $this->tong = $a+$b;
-        echo "Tong: $this->tong";
+    public function Sum($a, $b){
+        $c = $a + $b;
+        return $c;
     }
+}
 
-}
-class Con extends Cha
-{
-    function NhanDoi(){
-        echo "Gia tri phep nhan doi: ".$this->tong*2;
-    }
-}
-class Chau extends Cha
-{
-    function NhanBa(){
-        echo "Gia tri phep nhan ba: ".$this->tong*3;
-    }
-}
-class HT extends Cha
-{
-    function HT1(){
-        echo "Class HT";
-    }
-}
-$cha1 = new Cha;
-$cha1->Tong(2,5);
-$con1 = new Con;
-$con1->Tong(5,7);
-$chau1 = new Chau;
-$chau1->Tong(10,4);
-$h1 = new HT;
-$h1->Tong(5,5);
-?>
+$calc = new Caculator;
+echo $calc->Sum(4,5);
